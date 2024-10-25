55418fa14815eb4fa537779c4e3c077f94355e98
Abdul Hakim Akash <abdulhakimakash209@gmail.com>
2024-10-25 14:40:41 +0600

    Added Succesfully

---
+++ DS/Queue_implementation/Queue_by_list.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class myQueue
+{
+public:
+    list<int> l;
+    void push(int v)
+    {
+        l.push_back(v);
+    }
+    void pop()
+    {
+        l.pop_front();
+    }
+    int front()
+    {
+        return l.front();
+    }
+    int size()
+    {
+        return l.size();
+    }
+    bool empty()
+    {
+        return l.empty();
+    }
+};
+
+int main()
+{
+    myQueue q;
+    int n; cin>>n;
+     while(n--)
+     {
+        int x; cin>>x;
+         q.push(x);
+     }
+     while(!q.empty())
+     {
+        cout<<q.front() <<" ";
+        q.pop();
+     }
+     //cout<<q.front()<<" ";
+}
