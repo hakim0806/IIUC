5d38758c673ba74ea568da133a3de23b41532e84
Abdul Hakim Akash <abdulhakimakash209@gmail.com>
2025-01-18 16:38:18 +0600

    Add files via upload

---
+++ DS_Final/hashing_doublehashing.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> h;
+int t_size;
+int hashing_fun(int key) {
+    return key % t_size;
+}
+
+int double_hashing(int key) {
+    return 7 - (key % 7);  
+}
+
+void insert(int key) {
+    int idx = hashing_fun(key);
+    int step = double_hashing(key);
+    while (h[idx] != -1) {
+        idx = (idx + step) % t_size;
+    }
+    h[idx] = key;
+}
+
+void disply() {
+    for (int i = 0; i < t_size; i++) {
+        if (h[i] == -1) {
+            cout << i << " : " << "empty" << endl;
+        } else {
+            cout << i << " : " << h[i] << endl;
+        }
+    }
+}
+int main() {
+    cin >> t_size;
+    h.assign(t_size, -1);
+    int key;
+    while (cin >> key) {
+        if (key == -1) break;
+        insert(key);
+    }
+    disply();
+}
